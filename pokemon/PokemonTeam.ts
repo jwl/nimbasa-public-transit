d4606c7e1b0a2eaf1cb9612535ebea5631ca82ab
James Leung <james.warren.leung@gmail.com>
2018-12-30 18:29:08 -0800

    Initial implementation of PokemonTeam

---
+++ pokemon/PokemonTeam.ts
@@ -0,0 +1,39 @@
+class PokemonTeam {
+  // TODO: list of Pokemon objects, how to do lists in Typescript?
+  private team: Pokemon[];
+  private trainer: string;
+  private activePokemon: Pokemon;
+
+  constructor(team: Pokemon[], trainer: string) {
+    //TODO: implement error checks for team.length > 6 and an empty team
+    this.team = team;
+    this.trainer = trainer;
+    this.activePokemon = team[0];
+  }
+
+  public getActivePokemon(): Pokemon {
+    return this.activePokemon;
+  }
+
+  public getActivePokemonPosition(): number {
+    return this.team.indexOf(this.getActivePokemon());
+  }
+
+  public getTrainer(): string {
+    return this.trainer;
+  }
+
+  public switchActivePokemon(newActivePokemon: Pokemon): void {
+    // TODO: implement with error checking
+  }
+
+  public getPokemonFromList(position: number): Pokemon {
+    // TODO: error checking for bounds
+    // note that position will be this.team[position-1]
+    return this.team[position - 1];
+  }
+
+  public getPokemonList(): Pokemon[] {
+    return this.team;
+  }
+}
