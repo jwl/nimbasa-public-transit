3d5e15520255b383b2df4e1b624a143cbc1b026c
James Leung <james.warren.leung@gmail.com>
2019-01-07 12:38:24 -0800

    made action into an abstract class

--- src/actions/Action.ts
+++ src/actions/Action.ts
@@ -1,7 +1,7 @@
 import { Pokemon } from "../pokemon/Pokemon";
 import { Move } from "../moves/Move";
 
-export interface Action {
+export abstract class Action {
   /* Priority - used to determine action order based on lowest number.
     Owner - used to ensure actions are applied to the correct players
     Target - used to determine what pokemon to switch to or hit with an attack in an array
@@ -11,22 +11,88 @@ export interface Action {
   */
 
   //TODO: implement as an abstract class
-  priority: number;
-  trainer: string;
-  actor: Pokemon;
-  target: number;
-  allied: boolean;
-  moveInfo: Move;
-  actionType: string;
+  protected priority: number;
+  protected trainer: string;
+  protected actor: Pokemon;
+  protected target: number;
+  protected allied: boolean;
+  protected moveInfo: Move;
+  protected actionType: string;
 
-  getPriority(): number;
-  //TODO implement following method signatures:
-  /* public int getPriority(){return priority;}
-  public String getTrainer(){return trainer;}
-  public Pokemon getActor(){return actor;}
-  public int getTarget(){return target;}
-  public boolean getTeamFlag(){return allied;}
-  public String getActionType(){return actionType;}
-  public Move getMoveInfo(){return moveInfo;}
-  public void setPriority(int priority){this.priority = priority;} */
+  constructor(
+    priority: number,
+    trainer: string,
+    actor: Pokemon,
+    target: number,
+    allied: boolean,
+    moveInfo: Move,
+    actionType: string
+  ) {
+    this.priority = priority;
+    this.trainer = trainer;
+    this.actor = actor;
+    this.target = target;
+    this.allied = allied;
+    this.moveInfo = moveInfo;
+    this.actionType = actionType;
+    // this.setVariables(
+    //   priority,
+    //   trainer,
+    //   actor,
+    //   target,
+    //   allied,
+    //   moveInfo,
+    //   actionType
+    // );
+  }
+
+  public setVariables(
+    priority: number,
+    trainer: string,
+    actor: Pokemon,
+    target: number,
+    allied: boolean,
+    moveInfo: Move,
+    actionType: string
+  ) {
+    this.priority = priority;
+    this.trainer = trainer;
+    this.actor = actor;
+    this.target = target;
+    this.allied = allied;
+    this.moveInfo = moveInfo;
+    this.actionType = actionType;
+  }
+
+  public getPriority(): number {
+    return this.priority;
+  }
+
+  public getTrainer(): string {
+    return this.trainer;
+  }
+
+  public getActor(): Pokemon {
+    return this.actor;
+  }
+
+  public getTarget(): number {
+    return this.target;
+  }
+
+  public getTeamFlag(): boolean {
+    return this.allied;
+  }
+
+  public getActionType(): string {
+    return this.actionType;
+  }
+
+  public getMoveInfo(): Move {
+    return this.moveInfo;
+  }
+
+  public setPriority(priority: number): void {
+    this.priority = priority;
+  }
 }
