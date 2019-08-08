7dc61ee4ecce3cea8a543cf82d37264be84faa96
James Leung <james.warren.leung@gmail.com>
2019-08-07 23:41:57 -0700

    WIP

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,33 @@
-import { Router } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 
 type Wrapper = (router: Router) => void;
 
-export const applyMiddleWare = (middleware: Wrapper[], router: Router) => {
-  for (const f of middleware) {
-    f(router);
+export const applyMiddleWare = (
+  middlewareWrappers: Wrapper[],
+  router: Router
+) => {
+  for (const wrapper of middlewareWrappers) {
+    wrapper(router);
+  }
+};
+
+type Handler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<void> | void;
+
+type Route = {
+  path: string;
+  method: string;
+  handler: Handler | Handler[];
+};
+
+export const applyRoutes = (routes: Route[], router: Router) => {
+  for (const route of routes) {
+    const { method, path, handler } = route(router as any)[method](
+      path,
+      handler
+    );
   }
 };
